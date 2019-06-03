8cdb6d20e467492b29a4cf03e40f4c02459e799e
wujunze <itwujunze@163.com>
2019-06-03 16:41:29 +0800

    remove useless code

--- api/middlewares/cors.go
+++ api/middlewares/cors.go
@@ -12,7 +12,7 @@ func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		environment := strings.ToLower(env.Getenv("ENV"))
 		if environment != "production" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "http://local.yubadev.dz11.com:3000")
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 			c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
